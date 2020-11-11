0a6eed4be8a58c2e46770822d271350d0948e3f2
PatoSaly <saly.pato@gmail.com>
2020-11-11 22:32:04 +0100

    Doplnil som zoom funcionalitu

--- js/javascript.js
+++ js/javascript.js
@@ -26,6 +26,63 @@ document.addEventListener("DOMContentLoaded", () => {
         options: {
             legend: {
                 display: false
+            },
+            plugins: {
+                zoom: {
+                    // Container for pan options
+ 
+                    // Container for zoom options
+                    zoom: {
+                        // Boolean to enable zooming
+                        enabled: true,
+ 
+                        // Enable drag-to-zoom behavior
+                        drag: true,
+ 
+                        // Drag-to-zoom effect can be customized
+                        // drag: {
+                        // 	 borderColor: 'rgba(225,225,225,0.3)'
+                        // 	 borderWidth: 5,
+                        // 	 backgroundColor: 'rgb(225,225,225)',
+                        // 	 animationDuration: 0
+                        // },
+ 
+                        // Zooming directions. Remove the appropriate direction to disable
+                        // Eg. 'y' would only allow zooming in the y direction
+                        // A function that is called as the user is zooming and returns the
+                        // available directions can also be used:
+                        //   mode: function({ chart }) {
+                        //     return 'xy';
+                        //   },
+                        mode: 'xy',
+ 
+                        rangeMin: {
+                            // Format of min zoom range depends on scale type
+                            x: null,
+                            y: null
+                        },
+                        rangeMax: {
+                            // Format of max zoom range depends on scale type
+                            x: null,
+                            y: null
+                        },
+ 
+                        // Speed of zoom via mouse wheel
+                        // (percentage of zoom on a wheel event)
+                        speed: 0.1,
+ 
+                        // Minimal zoom distance required before actually applying zoom
+                        threshold: 2,
+ 
+                        // On category scale, minimal zoom level before actually applying zoom
+                        sensitivity: 3,
+ 
+                      /*  // Function called while the user is zooming
+                        onZoom: function({chart}) { console.log(`I'm zooming!!!`); },
+                        // Function called once zooming is completed
+                        onZoomComplete: function({chart}) { console.log(`I was zoomed!!!`); }*/
+                    }
+                }
             }
         }
     });
@@ -37,17 +94,18 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
         source.addEventListener("message", function(e) {
+            var value = document.getElementById("myForm").shadowRoot.querySelector('#rangeInput').value;
             var data = JSON.parse(e.data);
             chart.data.labels.push(data.x); 
             if($('#sin').is(":checked")) {
-                chart.data.datasets[0].data.push({x:data.x, y:data.y1});
+                chart.data.datasets[0].data.push({x:(data.x), y:(data.y1*value)});
                 chart.getDatasetMeta(0).hidden=false;
             } else {
                 chart.getDatasetMeta(0).hidden=true;
             }
             if($('#cos').is(":checked")) {
                 chart.getDatasetMeta(1).hidden=false;
-                chart.data.datasets[1].data.push({x:(data.x), y:(data.y2)});
+                chart.data.datasets[1].data.push({x:(data.x), y:(data.y2*value)});
             } else {
                 chart.getDatasetMeta(1).hidden=true;
             }
